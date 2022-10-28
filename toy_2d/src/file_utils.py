eae0d7192dc70f722b4355feaa52af2c21fb9f16
bibit <ebianchini.mit@gmail.com>
2022-10-28 14:38:01 -0400

    Avoid directory does not exist errors when saving created files

--- toy_2d/src/file_utils.py
+++ toy_2d/src/file_utils.py
@@ -7,3 +7,25 @@ import os
 ROOT_DIR = op.dirname(op.dirname(op.abspath(__file__)))
 TEMP_DIR = f'{ROOT_DIR}/tmp'
 OUT_DIR = f'{ROOT_DIR}/out'
+
+
+def assure_created(directory: str) -> str:
+    """A function from the DAIR PLL repository:  Wrapper to put around directory
+    paths which ensure their existence.
+
+    Args:
+        directory: Path of directory that may not exist.
+
+    Returns:
+        ``directory``, Which is ensured to exist by recursive mkdir.
+    """
+    directory = op.abspath(directory)
+    if not op.exists(directory):
+        assure_created(op.dirname(directory))
+        os.mkdir(directory)
+    return directory
+
+
+# Make sure the temp and out directories exist, if not already.
+assure_created(TEMP_DIR)
+assure_created(OUT_DIR)
