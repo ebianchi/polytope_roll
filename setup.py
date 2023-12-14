44a3b356e6e9c10594557b277167609de8b6dbfe
Bibit Bianchini <ebianchini.mit@gmail.com>
2023-12-14 14:06:58 -0500

    Comment out package line for polytope roll due to installation issues

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
                + f'optimization and controls.',
    author='Bibit Bianchini',
    author_email='bibit@seas.upenn.edu',
-   packages=['polytope_roll'],
+   #packages=['polytope_roll'],
    install_requires=[
       'gurobipy',
       'imageio',
